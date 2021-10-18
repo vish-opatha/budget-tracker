c48e44dc020da6ea80af82206e4c6a4d4223255d
Vish O <v.opatha@gmail.com>
2021-10-18 11:46:47 +1030

    Add formatting to server.js

--- server.js
+++ server.js
@@ -24,5 +24,4 @@ mongoose.connect("mongodb://localhost/budget", {
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+  console.log(`App running on port ${PORT}!`);});
