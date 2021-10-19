a77fda54bc14d044be1b5be35c3ad919fd27e49f
Vish O <v.opatha@gmail.com>
2021-10-19 11:52:36 +1030

    Add check database function which includes sending bulk requests to the database

--- public/db.js
+++ public/db.js
@@ -23,3 +23,48 @@ request.onupgradeneeded = function (e) {
 request.onerror = function (e) {
   console.log(`Woops! ${e.target.errorCode}`);
 };
+
+function checkDatabase() {
+  // Open a transaction on your BudgetTransactionStore db
+  let transaction = db.transaction(["BudgetTransactionStore"], "readwrite");
+
+  // access your BudgetTransactionStore object
+  const store = transaction.objectStore("BudgetTransactionStore");
+
+  // Get all records from store and set to a variable
+  const getAll = store.getAll();
+
+  // If the request was successful
+  getAll.onsuccess = function () {
+    // If there are items in the store, we need to bulk add them when we are back online
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          // If our returned response is not empty
+          if (res.length !== 0) {
+            // Open another transaction to BudgetTransactionStore with the ability to read and write
+            transaction = db.transaction(
+              ["BudgetTransactionStore"],
+              "readwrite"
+            );
+
+            // Assign the current store to a variable
+            const currentStore = transaction.objectStore(
+              "BudgetTransactionStore"
+            );
+
+            // Clear existing entries because our bulk add was successful
+            currentStore.clear();
+          }
+        });
+    }
+  };
+}
